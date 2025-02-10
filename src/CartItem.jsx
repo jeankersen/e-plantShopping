d6f1c7e181c3cc340c43d33126b587d7e79f2e8d
kersen <kersenferon@yahoo.com>
2025-02-10 17:46:27 -0500

    modi cartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,38 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
+  // Calcul du montant total du panier
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + item.quantity * item.cost, 0).toFixed(2);
   };
 
-  const handleContinueShopping = (e) => {
-   
-  };
-
-
-
-  const handleIncrement = (item) => {
-  };
-
-  const handleDecrement = (item) => {
-   
-  };
-
-  const handleRemove = (item) => {
-  };
-
-  // Calculate total cost based on quantity for an item
+  // Calcul du sous-total d'un article
   const calculateTotalCost = (item) => {
+    return (item.quantity * item.cost).toFixed(2);
+  };
+
+  // Gestion de la continuité des achats
+  const handleContinueShopping = () => {
+    onContinueShopping();
+  };
+
+  // Incrémenter la quantité d'un article
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  };
+
+  // Décrémenter la quantité d'un article
+  const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
+  };
+
+  // Supprimer un article du panier
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   return (
@@ -41,7 +50,7 @@ const CartItem = ({ onContinueShopping }) => {
             <img className="cart-item-image" src={item.image} alt={item.name} />
             <div className="cart-item-details">
               <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-cost">${item.cost}</div>
               <div className="cart-item-quantity">
                 <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
@@ -55,14 +64,12 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={() => alert('Functionality to be added for future reference')}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
