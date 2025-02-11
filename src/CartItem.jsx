589edbd345f60e971d5c6d2a23ce5252e7632067
kersen <kersenferon@yahoo.com>
2025-02-11 08:20:47 -0500

    add coming soon

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -64,7 +64,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={() => alert("Coming Soon!")} >Checkout</button>
       </div>
     </div>
   );
